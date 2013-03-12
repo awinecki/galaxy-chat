19fb5e90b2934a5b154a14bc364f9c924e833b9f
stickoman <winecki@gmail.com>
2013-03-12 03:48:29 +0100

    redesign part 2, added MIT License

--- client/assets/utilityFunctions.js
+++ client/assets/utilityFunctions.js
@@ -2,10 +2,19 @@
 // Useful functions
 
 // Parse string to make imgs from img links
-function parseLinks(string) {
+function parseImages(string) {
 	return string
-		.replace(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/gi, "<img src='$1' />");
+		.replace(/<a href=\'([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))'>.*<\/a>/gi, "<img src='$1' />");
 };
+
+function replaceURLWithHTMLLinks(string) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return string.replace(exp,"<a href='$1'>$1</a>"); 
+}
+
+function parseContent(string) {
+	return parseImages(replaceURLWithHTMLLinks(string));
+}
 
 function refreshTimes(interval) {
 	$(".single-message").each(function(i, val) {
--- client/client.js
+++ client/client.js
@@ -1,35 +1,37 @@
 
-// Initialize collections
+// Initialize mongoDB collections
 Users    = new Meteor.Collection("users");
 Messages = new Meteor.Collection("messages");
+
+var ENV = 'test';
+
+
+// function for scrolling chat view down
+function scrollDown(selector, selector2) {
+  $(selector).scrollTop($(selector2).height());
+}
 
 // Stuff to do if client
 if (Meteor.is_client) {
 
   // On client startup
   Meteor.startup(function() {
-
-    Session.set("username", "Andrzej");
-    //console.log(Users.find({}));
-    // Session.set("auth", false);
-    Session.set("auth", true);
-
+    Session.set("auth", false);
     refreshTimes(5000);
 
-    // Dev
-    var user = {
-      name: "awinecki",
-      color: "red"
-    }
-    if (user.name != '') {
-      $('#overlay-wrapper').fadeOut();
-      $('#top-nav-stripe').fadeIn();
+    if (ENV == 'development') {
+      var user = { 
+        name:  'awinecki',
+        color: '#2e95ff'
+      };
+      // set session
       Session.set("user", user);
       Session.set("auth", true);
-      $('#overlay-wrapper').fadeOut();
-      $('#top-nav-stripe').fadeIn();
-      //document.getElementById('send-msg-form').scrollIntoView(true); //ugly
+      // hide login box
+      $('#overlay-wrapper').fadeOut('fast');
+      // focus chat input box
       $('#input-text').focus();
+      // add user to chat 
       Meteor.call('createUser', {
         name:   user.name,
         color:  user.color,
@@ -40,66 +42,87 @@ if (Meteor.is_client) {
         Session.set('_id', data);
         Meteor.subscribe("users", data);
         Meteor.subscribe("messages");
-        setTimeout(function() {
+        setTimeout(function () {
           Messages.insert({
-            notification: true,
-            content: 'User connected.',
+            author:  user.name,
+            color:   user.color,
+            content: 'Hai there',
             time:    new Date().getTime()
-          });
-          Messages.insert({
-            author:  'awinecki',
-            color:   '#2e95ff',
-            content: 'hi there',
-            time:    new Date().getTime()
+          }, function() {
+            scrollDown('#chat-container', '.messages-wrapper');
           });
         }, 100);
-      })
-    }
+      });
+    } 
 
   });
+
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+  // TEMPLATES
+  // ----------------------------------------------------
 
   // loginBox template 
   Template.loginBox.events({
 
-    // Submit/ log in handler TODO: this is too long, 
-    // kick out some of this code
-		'submit #login-form': function (e) {
+    // color choice
+    'click div.color-box': function(e) {
+      var el = $(e.toElement);
+      $('#login-color').val(el.data('color')); // data
+      $('.color-box').removeClass('selected'); // visuals
+      el.addClass('selected');
+    },
+
+    // login to chat
+    'submit #login-form': function (e) {
       e.preventDefault();
-      var user = {
+
+      var user = { // get user data
         name:  $('#login-name').val(),
         color: $('#login-color').val()
       };
-      if (user.name != '') {
-        $('#overlay-wrapper').fadeOut();
-        $('#top-nav-stripe').fadeIn();
-        Session.set("user", user);
-        Session.set("auth", true);
-        $('#overlay-wrapper').fadeOut();
-        $('#top-nav-stripe').fadeIn();
-				//document.getElementById('send-msg-form').scrollIntoView(true); //ugly
-				$('#input-text').focus();
-				Meteor.call('createUser', {
-					name:   user.name,
-					color:  user.color,
-					online: true,
-					typing: false
-				}, function(err, data) {
-					if (err) console.log(err);
-					Session.set('_id', data);
-					Meteor.subscribe("users", data);
-					Meteor.subscribe("messages");
-				})
+
+      var formIsValid = (user.name && user.color); // form validation
+      var nameNotTooLong = (user.name.length < 19);
+
+      if (formIsValid) {
+        if (nameNotTooLong) {
+          // set session
+          Session.set("user", user);
+          Session.set("auth", true);
+          // hide login box
+          $('#overlay-wrapper').fadeOut();
+          // focus chat input box
+          $('#input-text').focus();
+          // add user to chat 
+          Meteor.call('createUser', {
+            name:   user.name,
+            color:  user.color,
+            online: true,
+            typing: false
+          }, function(err, data) {
+            if (err) console.log(err);
+            Session.set('_id', data);
+            Meteor.subscribe("users", data);
+            Meteor.subscribe("messages");
+          });
+        } else {
+          $('#login-errors').html('Username is too long (max 18 chars).');
+        }
+      } else {
+        $('#login-errors').html('Please choose a username and color.');
       }
 
     }
   });
 
+
   // chat template
   Template.chat.messages = function() {
     return Messages.find({}, {sort: {time: 1}});
   };
-  Template.chat.users = function() { 
-    return Users.find({}, {sort: {name: -1}}); 
+  Template.chat.users = function() {
+    return Users.find({}, {sort: {name: -1}});
   };
   Template.chat.username = function() {
     if (Session.get("auth")) {
@@ -110,8 +133,11 @@ if (Meteor.is_client) {
     }
   };
 
+  // userStripe template
+  Template.userStripe.user = function() { return Session.get("user"); };
+
   // usersbox template
-  Template.usersbox.users = function() { return Users.find({}, {}); };
+  Template.usersList.users = function() { return Users.find({}, {}); };
 
   // user template
   Template.user.notTyping = function() { return !this.typing; };
@@ -121,8 +147,6 @@ if (Meteor.is_client) {
 
   // Message template
 	Template.message.content = function() {
-		console.log(this.content);
-		console.log(unescape(this.content));
 		return unescape(this.content);
 	};
   Template.message.timeFormatted = function() {
@@ -135,20 +159,6 @@ if (Meteor.is_client) {
     return this.color || "009cde";
   };
   Template.message.events({
-  });
-
-  // authNavbox template
-  Template.authNavbox.username = function() {
-    if (Session.get("auth")) {
-      return Session.get("username");
-    } else {
-      return "nobody";
-    }
-  };
-  Template.authNavbox.events({
-    'click #input-nick': function(e) {
-      e.preventDefault();
-    }
   });
 
   // messageSendForm template
@@ -167,7 +177,9 @@ if (Meteor.is_client) {
 					Users.update({name: user.name}, {$set: {
 						typing: true,
 						liveMsg: text
-					}});
+					}}, function() {
+            scrollDown('#chat-container', '.messages-wrapper');
+          });
 				} else {
 					Users.update({name: user.name}, {$set: {typing: false}});
 				}
@@ -177,17 +189,30 @@ if (Meteor.is_client) {
     // send message handler
     'submit #send-msg-form': function(e) {
       e.preventDefault();
-      var user = Session.get("user");
       var content = $('#input-text').val();
-      var userFromDB = Users.findOne({name: user.name});
-      $('#notify-' + userFromDB._id).hide();
-      Messages.insert({
-        author:  user.name,
-        color:   user.color,
-				content: parseLinks(content),
-        time:    new Date().getTime()
-      });
-      $('#input-text').val('');
+      if (content) {
+        var user = Session.get("user");
+
+        Users.update({name: user.name}, {$set: {typing: false}});
+
+        $('#input-text').val('');
+        var userFromDB = Users.findOne({name: user.name});
+        $('#notify-' + userFromDB._id).hide();
+
+        Users.update({name: user.name}, {$set: {
+          typing: false,
+          liveMsg: '' 
+        }});
+
+        Messages.insert({
+          author:  user.name,
+          color:   user.color,
+  				content: parseContent(content),
+          time:    new Date().getTime()
+        }, function() {
+          scrollDown('#chat-container', '.messages-wrapper');
+        });
+      }
     }
     
   });
